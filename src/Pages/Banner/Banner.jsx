7e12d7b42f5d55057ea2aebecd605d7cfc32f6a2
Sumiyaakhi <sumiya.akhi793@gmail.com>
2023-05-19 12:05:29 +0600

    banner section updated

---
+++ src/Pages/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import img from '../../assets/Images/ezgif.com-webp-to-jpg.jpg'
+
+const Banner = () => {
+    return (
+        <div className='relative'>
+        <img className='w-full rounded' src={img} alt="" />
+       <div className='absolute right-5 top-1/3 text-white left-14 '>
+       <h1 className='text-3xl mb-6 font-serif'>Putting the HER in hero</h1>
+       <p className=''>Carefully crafted by artists across the Walt Disney company,<br /> each doll from the Disney Designer Collection <br /> celebrates a Disney Princess and her heroic story.</p>
+       </div>
+    </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import img from '../../../assets/Images/ezgif.com-webp-to-jpg.jpg'
+import Banner from '../../Banner/Banner';
 
 const Home = () => {
     return (
-        <div className='relative'>
-            <img className='w-full' src={img} alt="" />
-           <div className='absolute right-5 top-1/3 text-white left-14 '>
-           <h1 className='text-3xl mb-6 font-serif'>Putting the HER in hero</h1>
-           <p className=''>Carefully crafted by artists across the Walt Disney company,<br /> each doll from the Disney Designer Collection <br /> celebrates a Disney Princess and her heroic story.</p>
-           </div>
-        </div>
+       <Banner></Banner>
     );
 };
 
